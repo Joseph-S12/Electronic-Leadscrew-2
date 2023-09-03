24d0231e79910e13cdb26111ade1a7a20ecf55e8
Joseph-S12 <jjsisson14@gmail.com>
2023-09-03 17:43:35 +0100

    Added statement for switching between threading and dividing

--- leadscrew/main.c
+++ leadscrew/main.c
@@ -68,49 +68,54 @@ void main() {
 
 		case STATUS_STOPPED:
 			led_status |= 1;
-			if(gpio_switches.forward || gpio_switches.reverse) {
-				forward = gpio_switches.forward;
-				if(!forward) led_status |= 64;
+			if (gpio_switches.leadscrew){
+				if(gpio_switches.forward || gpio_switches.reverse) {
+					forward = gpio_switches.forward;
+					if(!forward) led_status |= 64;
 
-				if(replan && mode == MODE_METRIC) motion_plan_thread_metric((float)pitch_mm_100 / 100.0f);
-				if(replan && mode == MODE_IMPERIAL) motion_plan_thread_imperial((float)pitch_tpi_10 / 10.0f);
+					if(replan && mode == MODE_METRIC) motion_plan_thread_metric((float)pitch_mm_100 / 100.0f);
+					if(replan && mode == MODE_IMPERIAL) motion_plan_thread_imperial((float)pitch_tpi_10 / 10.0f);
 
-				replan = false;
+					replan = false;
 
-				if(gpio_switches.lhand) {
-					led_status |= 32;
-					motion_plan_direction(forward, true);
-					motion_run();
-				} else if(gpio_switches.rhand) {
-					motion_plan_direction(forward, false);
-					motion_run();
+					if(gpio_switches.lhand) {
+						led_status |= 32;
+						motion_plan_direction(forward, true);
+						motion_run();
+					} else if(gpio_switches.rhand) {
+						motion_plan_direction(forward, false);
+						motion_run();
+					}
+				} else {
+					if(gpio_switches.metric) {
+						led_status |= 16;
+						if(gpio_switches.increase) pitch_mm_100 += 5;
+						if(gpio_switches.decrease) pitch_mm_100 -= 5;
+						if(pitch_mm_100 < 20) pitch_mm_100 = 20;
+						if(pitch_mm_100 > 2500) pitch_mm_100 = 2500;
+
+						if(mode != MODE_METRIC || gpio_switches.increase || gpio_switches.decrease)
+							replan = true;
+
+						updatePitch(pitch_mm_100, 2);
+						mode = MODE_METRIC;
+					} else if(gpio_switches.imperial) {
+						led_status |= 8;
+						if(gpio_switches.increase) pitch_tpi_10 += 5;
+						if(gpio_switches.decrease) pitch_tpi_10 -= 5;
+						if(pitch_tpi_10 < 10) pitch_tpi_10 = 10;
+						if(pitch_tpi_10 > 800) pitch_tpi_10 = 800;
+
+						if(mode != MODE_IMPERIAL || gpio_switches.increase || gpio_switches.decrease)
+							replan = true;
+
+						updatePitch(pitch_tpi_10, 1);
+						mode = MODE_IMPERIAL;
+					}
 				}
-			} else {
-				if(gpio_switches.metric) {
-					led_status |= 16;
-					if(gpio_switches.increase) pitch_mm_100 += 5;
-					if(gpio_switches.decrease) pitch_mm_100 -= 5;
-					if(pitch_mm_100 < 20) pitch_mm_100 = 20;
-					if(pitch_mm_100 > 2500) pitch_mm_100 = 2500;
-
-					if(mode != MODE_METRIC || gpio_switches.increase || gpio_switches.decrease)
-						replan = true;
-
-					updatePitch(pitch_mm_100, 2);
-					mode = MODE_METRIC;
-				} else if(gpio_switches.imperial) {
-					led_status |= 8;
-					if(gpio_switches.increase) pitch_tpi_10 += 5;
-					if(gpio_switches.decrease) pitch_tpi_10 -= 5;
-					if(pitch_tpi_10 < 10) pitch_tpi_10 = 10;
-					if(pitch_tpi_10 > 800) pitch_tpi_10 = 800;
-
-					if(mode != MODE_IMPERIAL || gpio_switches.increase || gpio_switches.decrease)
-						replan = true;
-
-					updatePitch(pitch_tpi_10, 1);
-					mode = MODE_IMPERIAL;
-				}
+			}
+			else if (gpio_switches.dividing){
+				
 			}
 			break;
 
